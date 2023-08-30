4d54323f177850457c9dcf9741f608f895bb0ef2
ItsVibhanshuSharma <vibhanshusharma1811@gmail.com>
2023-08-30 12:34:27 +0530

    First Commint

---
+++ App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Button, Linking, View} from 'react-native';
+
+const App = () => {
+  const call = () => {
+    Linking.openURL('tel: + 7669785550');
+  };
+
+  const whatsApp = () => {
+    Linking.openURL('whatsapp://send?phone=7669785550');
+  };
+
+  const openGmail = () => {
+    Linking.openURL('mailto:codervibhanshusharma.com');
+  };
+
+  const openInstagramProfile = () => {
+    Linking.openURL('instagram://user?username=theishubhaiya');
+  };
+
+  return (
+    <View>
+      <Button title="call" onPress={() => call()} />
+
+      <Button title="Whats App" onPress={() => whatsApp()} />
+
+      <Button title="Gmail" onPress={() => openGmail()} />
+      <Button title="Instagram" onPress={() => openInstagramProfile()} />
+    </View>
+  );
+};
+
+export default App;
